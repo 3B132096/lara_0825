14ffc1bafe6c2064711c0fa3539e8f190c4e8276
葉佳豪 <3B132096@gm.student.ncut.edu.tw>
2024-11-09 12:24:53 +0800

    用save的方式新增資料

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $post = new Post();
+    $post->title = 'test title';
+    $post->content = 'test content';
+    $post->save();
 });
