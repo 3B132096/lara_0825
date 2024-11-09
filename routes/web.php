c13fd5ecfe3d1af1d474ce5184b9e59a01fc2c50
葉佳豪 <3B132096@gm.student.ncut.edu.tw>
2024-11-09 14:53:49 +0800

    多筆資料的集合

--- routes/web.php
+++ routes/web.php
@@ -63,6 +63,9 @@ Route::get('/', function () {
 
     //Post::destroy(3, 5, 7);
 
-    $allPosts = Post::all();
-    dd($allPosts);
+    //$allPosts = Post::all();
+    //dd($allPosts);
+
+    $featuredPosts = Post::where('is_feature', 1)->get();
+    dd($featuredPosts);
 });
