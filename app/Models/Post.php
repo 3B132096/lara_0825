cac14295d1dfc1b636f8dc8228afe0ec4921cc32
葉佳豪 <3B132096@gm.student.ncut.edu.tw>
2024-11-12 22:56:58 +0800

    Post透過comments()，擷取其所有評論($comments)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Comment;
 
 class Post extends Model
 {
@@ -13,4 +14,8 @@ class Post extends Model
         'content',
         'is_feature'
     ];
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
 }
