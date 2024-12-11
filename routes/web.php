860c5d22b1558364db8b25ce012668663094a3d6
葉佳豪 <3B132096@gm.student.ncut.edu.tw>
2024-12-11 16:31:54 +0800

    練習條件式

--- routes/web.php
+++ routes/web.php
@@ -33,14 +33,17 @@ Route::get('/', function () {
     //echo '內容： '.$post->content.'<br>';
     //dd($post);
 
-    $posts = Post::all();
+    //$posts = Post::all();
 
-    foreach ($posts as $post) {
-        echo '編號： '.$post->id.'<br>';
-        echo '標題： '.$post->title.'<br>';
-        echo '內容： '.$post->content.'<br>';
-        echo '張貼時間： '.$post->created_at.'<br>';
-        echo '---------------------------'.'<br>';
-    }
+    //foreach ($posts as $post) {
+        //echo '編號： '.$post->id.'<br>';
+        //echo '標題： '.$post->title.'<br>';
+        //echo '內容： '.$post->content.'<br>';
+        //echo '張貼時間： '.$post->created_at.'<br>';
+        //echo '---------------------------'.'<br>';
+    //}
+    //dd($posts);
+
+    $posts = Post::where('id','<','10')->orderBy('id','DESC')->get();
     dd($posts);
 });
