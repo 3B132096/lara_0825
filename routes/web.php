96d25ac1f7a29f2d057b099233c73e3f4e20b5c9
葉佳豪 <3B132096@gm.student.ncut.edu.tw>
2024-12-11 16:38:44 +0800

    使用save方法更新資料

--- routes/web.php
+++ routes/web.php
@@ -48,8 +48,12 @@ Route::get('/', function () {
     //dd($posts);
 
     $post = Post::find(1);
-    $post->update([
-        'title' => 'updated title',
-        'content' => 'updated content',
-    ]);
+    //$post->update([
+        //'title' => 'updated title',
+        //'content' => 'updated content',
+    //]);
+
+    $post->title = 'saved title';
+    $post->content = 'saved content';
+    $post->save();
 });
