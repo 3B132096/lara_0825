8fcd1411e918a0937134be525abe056d83f17952
葉佳豪 <3B132096@gm.student.ncut.edu.tw>
2024-12-11 16:05:39 +0800

    用create的方式新增資料

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //return view('welcome');
-    $post = new Post();
-    $post->title = "test title";
-    $post->content = "test content";
-    $post->save();
+    //$post = new Post();
+    //$post->title = "test title";
+    //$post->content = "test content";
+    //$post->save();
+
+    Post::create([
+        'title' => 'created title',
+        'content' => 'created content',
+    ]);
 });
