23867a364c0b28788769501cf7ba7e13fb073734
葉佳豪 <3B132096@gm.student.ncut.edu.tw>
2024-12-11 16:36:03 +0800

    使用update方法更新資料

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,12 @@ Route::get('/', function () {
     //}
     //dd($posts);
 
-    $posts = Post::where('id','<','10')->orderBy('id','DESC')->get();
-    dd($posts);
+    //$posts = Post::where('id','<','10')->orderBy('id','DESC')->get();
+    //dd($posts);
+
+    $post = Post::find(1);
+    $post->update([
+        'title' => 'updated title',
+        'content' => 'updated content',
+    ]);
 });
